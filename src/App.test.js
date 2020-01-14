0e4b92d7e59b4a76c7a3303044673404f81254a3
plusz <Konto@Github410>
2020-01-14 23:20:58 +0100

    travis CI configured

--- src/App.test.js
+++ src/App.test.js
@@ -8,7 +8,7 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
+test('renders learn react link copy', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
